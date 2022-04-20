336dd9891f303ef3dd881afdf32d4860f73973da
snorresovold <73354016+snorresovold@users.noreply.github.com>
2022-04-20 09:36:51 +0200

    started work on the design itself

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from tkinter import Tk
+from tkinter import Tk, Label
 from server import Server
 
 tk = Tk()
 server = Server()
 
-
+# setup
 tk.title("snorres chatbot")
+tk.resizable(height= False, width = False)
+tk.geometry("500x500")
+
+HEIGHT, WIDTH = 500, 500
+
+# design
+x = Label(text="Test", fg="black", bg="white")
+x.place(x = HEIGHT/2, y = WIDTH/2)
 
 tk.mainloop()
