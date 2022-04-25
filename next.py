c4aa8996168b2ce049caeb9b5f4469f2aca19a14
snorresovold <73354016+snorresovold@users.noreply.github.com>
2022-04-25 12:36:13 +0200

    updates

---
+++ next.py
@@ -0,0 +1,69 @@
+from distutils.command.clean import clean
+from pdb import Restart
+from tkinter import*
+from turtle import clearscreen, st
+from server import *
+
+main = Tk()
+server = Server()
+
+#Window
+main.title("Chatbot 1IM")
+main.geometry("400x520")
+main.resizable(height=FALSE, width=FALSE)
+
+#Icon
+icon = PhotoImage(file = "Icon.png")
+main.iconphoto(False, icon)
+
+#Menu
+main_menu = Menu(main)
+file_menu = Menu(main)
+main_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Quit", command=main.destroy)
+main_menu.add_command(label="Quit", command=main.destroy)
+main.config(menu=main_menu)
+
+#Chat
+txtChat = Text(main, bg="#222230", foreground="#d1d2eb")
+txtChat.place(x=6, y=6, height=418, width=367)
+
+#Input
+entInput = Entry(main, width=44, bg="#222230", foreground="#d1d2eb")
+entInput.place(x=6, y=430, height=64)
+
+WORDLIST = ["what", "hva", "ka", "hvilken", "kor", "where", "er", "is", "?", "a", "i", "in"]
+
+#Send Text
+def send():
+    response_input = entInput.get()
+    storedInput = "You> " + entInput.get()
+
+    for x in response_input.split(): # removes uneccesary words from the bot
+        x.lower()
+        if x in WORDLIST:
+            print("deleting", x)
+            response_input = x.replace(x, "")
+        else:
+            print("not deleting", x)
+            lol = x
+
+
+    response_input.replace(" ", "")
+    print("response", lol)
+            
+    txtChat.insert(END, "\n" + storedInput)
+    answer = server.request(lol)
+    answer = "\nBot> " + answer
+    txtChat.insert (END, answer)
+
+#Send Button
+btnSend = Button(main, text=("SEND"), bg="#222230", font=("Times", "24", "bold"), foreground="#d1d2eb", activebackground="#d1d2eb",activeforeground="#222230", command=send)
+btnSend.place(x=278,y=430)
+
+#Scrollbar
+scrollbar = Scrollbar(main, command=txtChat.yview, cursor="heart")
+scrollbar.place(x=375, y=5, height = 420)
+
+main.mainloop()
--- server.py
+++ server.py
@@ -1,14 +1,24 @@
 class Server:
     # Hvis nokon sender X respons så får de Y svar
     relations = {
-        "python": "Python er et programmerings språk utviklet i 1989 av Guido van Rossum"
+        "python": "Python er et programmerings språk utviklet i 1989 av Guido van Rossum",
+        "string": "String is a collection of alphabets, words or other characters. It is one of the primitive data structures and are the building blocks for data manipulation. Python has a built-in string class named str . Python strings are 'immutable' which means they cannot be changed after they are created",
+        "strings": "String is a collection of alphabets, words or other characters. It is one of the primitive data structures and are the building blocks for data manipulation. Python has a built-in string class named str . Python strings are 'immutable' which means they cannot be changed after they are created",
+        "int":"In Python, integers are zero, positive or negative whole numbers without a fractional part and having unlimited precision, e.g. 0, 100, -10. The followings are valid integer literals in Python.",
+        "integer":"In Python, integers are zero, positive or negative whole numbers without a fractional part and having unlimited precision, e.g. 0, 100, -10. The followings are valid integer literals in Python.",
+        "bool": "In programming you often need to know if an expression is True or False. You can evaluate any expression in Python, and get one of two answers, True or False. When you compare two values, the expression is evaluated and Python returns the Boolean answer",
+        "boolean": "In programming you often need to know if an expression is True or False. You can evaluate any expression in Python, and get one of two answers, True or False. When you compare two values, the expression is evaluated and Python returns the Boolean answer",
+        "function": "A function is a block of code which only runs when it is called. You can pass data, known as parameters, into a function. A function can return data as a result",
     }
 
+    """
     def request(self, input):
         input.lower() # makes the responses none case sensitive
 
         for k, v in self.relations.items(): # we get an input from the dict, then return the opposite
-            if k == input:
+            if input in self.relations:
                 return v
             else:
+                print("not", k, v)
                 return "This response was invalid"
+    """
